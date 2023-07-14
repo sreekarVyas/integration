6deafb7cb9d3f0e4fad7cce039c68f8f79482102
Garvit Shah <garvitshah@gobbeldy-gook.local>
2023-07-14 18:25:36 +0530

    final-working

--- Users/urls.py
+++ Users/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.getUsers)
+    path('', views.getUsers, name="users"),
+    path('new-user/', views.entryForm, name="new-user")
 ]
--- Users/views.py
+++ Users/views.py
@@ -7,12 +7,30 @@ from django.shortcuts import render
 import json
 
 # Create your views here.
+
+
 @api_view(['GET'])
 def getUsers(request):
-    if request.method == 'GET':    
+    if request.method == 'GET':
         data = UserData.objects.all()
         # data = json.dumps(data)
-        serializer = UserDataSerializer(data, many = True)
+        serializer = UserDataSerializer(data, many=True)
         # return render(request, 'index.html', {"name": "string"})
+        return JsonResponse(serializer.data, safe=False)
+    return render(request, 'index.html')
+
+
+@api_view(['POST'])
+def entryForm(request):
+    if request.method == 'POST':
+        data = json.loads(request.body)
+        name = data['Name']
+        age = data['Age']
+        UserData.objects.create(
+            name=name,
+            age=age,
+        )
+        data = UserData.objects.all()
+        serializer = UserDataSerializer(data, many=True)
         return JsonResponse(serializer.data, safe=False)
     return render(request, 'index.html')
