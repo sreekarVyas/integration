c0bc3a1eb0a10b7db0297978fb4ac3c52bd6b6d3
Garvit Shah <garvitshah@gobbeldy-gook.local>
2023-07-14 18:26:26 +0530

    final-working

---
+++ reactapp/src/App.js
@@ -0,0 +1,32 @@
+import './App.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import RootLayout from './components/RootLayout';
+import { Children } from 'react';
+import Users from './components/Users';
+import Form from './components/form/Form';
+
+function App() {
+	const routerObject = createBrowserRouter([
+		{
+			path: '/',
+			element: <RootLayout />,
+			children: [
+				{
+					path: '/users',
+					element: <Users />,
+				},
+				{
+					path: '/form',
+					element: <Form />,
+				},
+			],
+		},
+	]);
+	return (
+		<>
+			<RouterProvider router={routerObject}></RouterProvider>
+		</>
+	);
+}
+
+export default App;
