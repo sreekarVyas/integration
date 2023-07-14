58b9ce9425795aa0057898b648b5670cb11250b1
Sreekar <kurudisreekar@gmail.com>
2023-07-14 15:58:02 +0530

    first commit

---
+++ Users/serializers.py
@@ -0,0 +1,7 @@
+from rest_framework.serializers import ModelSerializer
+from .models import UserData
+
+class UserDataSerializer(ModelSerializer):
+    class Meta:
+        model = UserData
+        fields = '__all__'
---
+++ Users/views.py
@@ -0,0 +1,18 @@
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from django.http import JsonResponse
+from .models import UserData
+from .serializers import UserDataSerializer
+from django.shortcuts import render
+import json
+
+# Create your views here.
+@api_view(['GET'])
+def getUsers(request):
+    if request.method == 'GET':    
+        data = UserData.objects.all()
+        # data = json.dumps(data)
+        serializer = UserDataSerializer(data, many = True)
+        # return render(request, 'index.html', {"name": "string"})
+        return JsonResponse(serializer.data, safe=False)
+    return render(request, 'index.html')
